854ac2e6a0d3fd4dc12289402a975b1e5740131b
Thomas Johnson <thomasij813@gmail.com>
2016-03-02 10:39:49 -0800

    Implement MD preview

--- client/script.js
+++ client/script.js
@@ -1,29 +1,44 @@
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {userInput: ''};
+  }
+
+  updateUserInput(event) {
+    const md = marked(event.target.value.toString(), {sanitize: true});
+    this.setState({ userInput: md });
+  }
+
   render() {
     return (
       <div className="body">
-        <UserForm />
-        <Preview />
+        <UserForm updateInputHandler={this.updateUserInput.bind(this)}/>
+        <Preview userInput={this.state.userInput}/>
       </div>
     )
   }
 }
 
 class UserForm extends React.Component {
+
   render() {
     return (
       <div className="userForm">
-        <textarea autoFocus></textarea>
+        <textarea autoFocus onChange={this.props.updateInputHandler}></textarea>
       </div>
     )
   }
 }
 
 class Preview extends React.Component {
+  rawMarkup(md) {
+    return {__html: md};
+  }
+
   render() {
     return (
       <div className="preview">
-        <p>This is the Preview component.</p>
+        <span dangerouslySetInnerHTML={this.rawMarkup(this.props.userInput)} />
       </div>
     )
   }
