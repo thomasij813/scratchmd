db583c12762bab736a9791752255b3142e5bdecf
Thomas Johnson <thomasij813@gmail.com>
2016-03-01 23:09:28 -0800

    Finish basic design

--- client/script.js
+++ client/script.js
@@ -1,6 +1,31 @@
 class App extends React.Component {
   render() {
-    return <h1>Hello, World!</h1>
+    return (
+      <div className="body">
+        <UserForm />
+        <Preview />
+      </div>
+    )
+  }
+}
+
+class UserForm extends React.Component {
+  render() {
+    return (
+      <div className="preview">
+        <p>This is the UserForm component.</p>
+      </div>
+    )
+  }
+}
+
+class Preview extends React.Component {
+  render() {
+    return (
+      <div className="userForm">
+        <p>This is the Preview component.</p>
+      </div>
+    )
   }
 }
 
