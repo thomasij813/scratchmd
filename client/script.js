4b02633ac8aaaa31f3fddd4d5cbbc02f92a01f51
Thomas Johnson <thomasij813@gmail.com>
2016-03-02 16:22:40 -0800

    Finish styles and add default md text

--- client/script.js
+++ client/script.js
@@ -1,7 +1,27 @@
+const defaultMD = `# Scratch.md
+### A very simple Markdown previewer
+
+---
+
+Instructions:
+
+1. Type your markdown in the left pane.
+2. See the changes in the right pane.
+3. ?????
+4. Profit!
+
+---
+
+If you need to learn how to get started with Markdown, [read this](http://daringfireball.net/projects/markdown/basics).
+
+This project was completed as part of [FreeCodeCamp's JavaScript curriculum](http://www.freecodecamp.com).
+
+Be sure to checkout [WriteMe.md](http://writeme.mattstow.com/), which was a visual inspiration for this project.`;
+
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {userInput: ''};
+    this.state = {userInput: marked(defaultMD, {sanitize: true})};
   }
 
   updateUserInput(event) {
@@ -24,7 +44,7 @@ class UserForm extends React.Component {
   render() {
     return (
       <div className="userForm">
-        <textarea autoFocus onChange={this.props.updateInputHandler}></textarea>
+        <textarea autoFocus onChange={this.props.updateInputHandler} defaultValue={defaultMD}></textarea>
       </div>
     )
   }
